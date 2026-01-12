9af15c05b813dddda68d13febc6fa20ae90cbcc7
sandeepmukku <sandeep12akp@gmail.com>
2026-01-13 02:07:04 +0530

    Update routes for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import ProtectedRoute from './components/ProtectedRoute';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Products';
+import Categories from './pages/Categories';
+import Suppliers from './pages/Suppliers';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-
-        {/* Protected Routes with Sidebar */}
-        <Route element={<ProtectedRoute />}>
-          <Route element={<Sidebar />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            {/* More pages here */}
-          </Route>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      
+      {/* All Dashboard pages go inside here */}
+      <Route element={<ProtectedRoute />}>
+        <Route element={<Sidebar />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/suppliers" element={<Suppliers />} />
+          {/* Add Profile and Reports routes here */}
         </Route>
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 }
 
