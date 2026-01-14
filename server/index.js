276b28ec4ab857eb57d27d805bc949be5985c625
sandeepmukku <sandeep12akp@gmail.com>
2026-01-15 03:40:16 +0530

    Configure backend for render and Add CORS

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://stocksync-iota.vercel.app/",
+    "https://stocksync-iota.vercel.app",
   ],
   credentials: true
 }));
